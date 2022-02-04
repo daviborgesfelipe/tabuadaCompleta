ac36f44e665f7544509584c1937c86fe56d59f5a
Davi Borges <davifelipe_300@hotmail.com>
2022-02-04 17:18:43 -0300

    Tabuada de 1 a 10 com instrução while alinhada.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace tabuadaCompleta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int multiplicador = 1;
+            int multiplicando = 1;
+            int resultado;
+
+            while (multiplicador < 10)
+            {
+                Console.WriteLine($"\n##### Tabuada do numero: {multiplicador} #####\n");
+
+                while (multiplicando <= 10)
+                {
+                    resultado = multiplicando * multiplicador;
+                    Console.WriteLine($"{multiplicador} x {multiplicando} = {resultado}");
+                    multiplicando = multiplicando + 1;
+                }
+
+                multiplicador = multiplicador + 1;
+                multiplicando = 1;
+            }
+        }
+    }
+}
